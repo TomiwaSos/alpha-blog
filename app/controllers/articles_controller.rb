df6c16462fe1054a957985690b91a292bab653bc
Tomiwa Sosanya <tomiwa@doyoucustoms.com>
2021-07-09 15:54:32 +0100

    articles are now editable

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,6 +10,10 @@ class ArticlesController < ApplicationController
 
     def new 
         @article = Article.new
+    end 
+
+    def edit
+        @article = Article.find(params[:id])
     end
 
     def create
@@ -20,6 +24,16 @@ class ArticlesController < ApplicationController
         else
             render 'new' 
         end
+    end 
+
+    def update 
+        @article = Article.find(params[:id])
+       if  @article.update(params.require(:article).permit(:title,:description))
+            flash[:notice] = "Article was updated successfuly."
+            redirect_to @article
+       else
+            render 'edit'
+       end
     end 
 
 end
