8e9e328cf0d6aeba4865134be9e115519982073a
Tomiwa Sosanya <tomiwa@doyoucustoms.com>
2021-07-13 13:49:28 +0100

    added edit functionaility

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,21 @@ class UsersController < ApplicationController
         @user = User.new
     end 
 
+    def edit 
+        @user = User.find(params[:id])
+    end 
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            flash[:notice] = "Your account information was successfully updateded"
+            redirect_to articles_path
+        else
+            render 'edit'
+        end
+    end
+
+
     def create 
         @user = User.new(user_params)
         if @user.save
@@ -14,6 +29,7 @@ class UsersController < ApplicationController
         end
     end 
 
+    
     private 
     def user_params
         params.require(:user).permit(:username, :email, :password)
