44f1ab0952d4b7cf9c4129977b2c3907b3db2b87
Tomiwa Sosanya <tomiwa@doyoucustoms.com>
2021-07-14 13:35:14 +0100

    Added tests for categories controller'

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,8 +1,37 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
+
+    def setup
+        @category = Category.new(name: 'sports')
+    end 
+
    test "Category should be valid" do
-       @category = Category.new(name: 'sports')
        assert @category.valid? 
    end
+
+   test 'Name should be present' do 
+        @category.name = ' '
+        assert_not @category.valid? 
+   end 
+
+   test 'name should be unique' do 
+        @category.save 
+        @category2 = Category.new(name: 'sports')
+        assert_not @category2.valid?
+
+   end 
+
+   test 'name shouldnt be too long' do 
+        @category.name = 'a' * 26
+        assert_not @category.valid?
+   end 
+
+   test 'name shouldnt be too short' do 
+        @category.name = 'a' * 2
+        assert_not @category.valid?
+
+   end 
+
+
 end
